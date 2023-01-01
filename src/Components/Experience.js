71beb09ea68e636a544f010676b15ed3513492a1
Nehal Borole <nehal.borole@gmail.com>
2023-01-01 10:38:10 +0530

    modified experience details

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -4,32 +4,39 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import WorkIcon from "@material-ui/icons/Work";
+import Chip from "@material-ui/core/Chip";
 
 export default function Experience() {
   const items = [
     {
       date: "June 2022 - August 2022",
+      technologies: ["Java", "Kotlin", "Splunk"],
       cardTitle: "BlackRock",
       cardSubtitle: "Software Engineering Intern",
-      // cardDetailedText: [
-      //   "Developing new security detections and platform empowering service teams.",
-      // ].map(function (text) {
-      //   return <li>{text}</li>;
-      // }),
+      cardDetailedText: [
+        "Enhanced a functionality for tax liability by introducing one-sided slack variables which are used in the portfolio optimization problem.",
+        "Developed a Splunk Dashboard for the clients team which tracks healthiness and displays internal performance breakdown of the proposal services used for producing client reports and debugging."
+      ].map(function (text) {
+        return <li>{text}</li>;
+      }),
     },
     {
       date: "July 2018 – July 2021",
+      technologies: ["Java", "React", "Spring Boot", "JPA", "CockroachDB", "Consul", "Kafka", "ELK"],
       cardTitle: "UBS",
       cardSubtitle: "Software Engineer",
       cardDetailedText: [
-        "Collected JSON data by web-scraping, using open API end points and storing it in a RDBMS.",
-        "Responsible for building SKU Classification system using the collected data.",
+        "Developed a microservices based programming platform which focused on availability and performance for UBS-specific programming contest for 5k+ employees. Event driven application which has Kafka as the backbone.",
+        "Developed generic React based components for trading violations based application which were used by the entire development team. These components significantly reduced the size of bundle and load time. Improved business engagement by migrating legacy application meant for external money managers to ReactJS.",
+        "Implemented a multithreaded functionality to upload financial models which reduced the upload time and made the UI non-blocking. Built and maintained RESTful APIs that were utilized for validations in the Advisory application. Increased test coverage using JUnit and Mockito.",
+
       ].map(function (text) {
         return <li>{text}</li>;
       }),
     },
     {
       date: "September 2017 - June 2018",
+      technologies: ["C++", , "ROS", "Javascript"],
       cardTitle: "Theta Controls",
       cardSubtitle: "Intern",
       cardDetailedText: [
@@ -95,6 +102,11 @@ export default function Experience() {
                     >
                       {data.cardDetailedText}
                     </p>
+                    <div className="experience-technologies">
+                    {data.technologies.map((name, i) => (
+                      <Chip key={i} label={name}></Chip>
+                    ))}
+                  </div>
                   </VerticalTimelineElement>
                 );
               })}
